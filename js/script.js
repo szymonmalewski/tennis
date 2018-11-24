bfebc302a85df042a57852211299ec7265011c22
Szymon Malewski <malewskiszymon@gmail.com>
2018-11-24 12:19:58 +0100

    Add all

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var canvasContext;
 
 var ballX = 400;
 var ballY = 300;
-var ballSpeedX = 10;
+var ballSpeedX = 10; // org 10
 var ballSpeedY = 5;
 var ballWidth = 50;
 
@@ -11,8 +11,11 @@ var paddle1Y = 250;
 var paddle2Y = 250;
 var userPaddleHeight = 100;
 var computerPaddleHeight = 100;
-const PADDLE_WIDTH = 10;
+const PADDLE_WIDTH = 5;
 var computerSpeed = 6;
+
+var ballColor = 'white';
+var backgroundPattern = "./images/darkness.png";
 
 var player1Score = 0;
 var player2Score = 0;
@@ -20,20 +23,133 @@ var WINNING_SCORE = 5;
 
 var showingWinScreen = false;
 
+var text;
+
+
 $(function(){
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 
+	$('input[name="gameButton"').click(function(){
+	    var framesPerSecond = 30;
+		setInterval(callBoth, 1000/framesPerSecond );
 
-	var framesPerSecond = 30;
-	setInterval(callBoth, 1000/framesPerSecond );
+		$( ".startButton" ).hide();
+	});
 
-	$('input[type="radio"]:checked').parent().css("background-color", "#aaaa55");
+	function myName() {
+	    text = document.getElementById("searchTxt").value;
+	    console.log(text.length);
+	    if (text.length > 0) {
+	    	 document.getElementById("userName").innerHTML = text;
+	    } else {
+	    	document.getElementById("userName").innerHTML = 'Noname';
+	    }
+	}
+
+	var input = document.getElementById("searchTxt");
+	input.addEventListener("keyup", function(event) {
+	    event.preventDefault();
+	    if (event.keyCode === 13) {
+	        document.getElementById("enterButton").click();
+	    }
+	});
+
+	$('button[id="enterButton"]').click(function(){
+		$( ".welcome" ).hide( "slow" );
+		myName();
+	});
+
+	function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+
+	$('button[class="settings"]').click(function(){
+		toggle_visibility('settingsOption');
+	});
+	// color change buttons START
+	$('input[name="colorPicker"]:checked').parent().css("border", "3px solid #4CAF50");
+
+	$('.color').on('click', function(){
+		$('input[name="colorPicker"]:checked').parent().css("border", "3px solid #4CAF50");
+		$('input[name="colorPicker"]:not(:checked)').parent().css("border", "1px solid black");
+	});
+
+	$('.color1').on('click', function(){
+		ballColor = 'white';
+	});
+
+	$('.color2').on('click', function(){
+		ballColor = 'yellow';
+	});
+
+	$('.color3').on('click', function(){
+		ballColor = 'orange';
+	});
+
+	$('.color4').on('click', function(){
+		ballColor = 'red';
+	});
+
+	$('.color5').on('click', function(){
+		ballColor = 'green';
+	});
+
+	$('.color6').on('click', function(){
+		ballColor = 'lightblue';
+	});
+
+	$('.color7').on('click', function(){
+		ballColor = 'blue';
+	});
+
+	$('.color8').on('click', function(){
+		ballColor = 'purple';
+	});
+
+	$('.color9').on('click', function(){
+		ballColor = 'grey';
+	});
+
+	// color change buttons END
+
+	//  background change buttons START
+
+	$('input[name="BackgroundPicker"]:checked').parent().css("border", "3px solid #4CAF50");
+
+	$('.background').on('change', function(){
+		$('input[name="BackgroundPicker"]:checked').parent().css("border", "3px solid #4CAF50");
+		$('input[name="BackgroundPicker"]:not(:checked)').parent().css("border", "1px solid black");
+	});
+
+	$('.background1').on('click', function(){
+		backgroundPattern = "./images/darkness.png";
+	});
+
+	$('.background2').on('click', function(){
+		backgroundPattern = "./images/wheat.png";
+	});
+
+	$('.background3').on('click', function(){
+		backgroundPattern = "./images/folk-pattern-black.png";
+	});
+
+	$('.background4').on('click', function(){
+		backgroundPattern = "./images/dark-grey-terrazzo.png";
+	});
+
+	//  background change buttons START
+
+	$('input[name="selection"]:checked').parent().css("background-color", "#97c499");
 
 	$('.button1').on('click', function(){
 		ballReset();
-		$('input[type="radio"]:checked').parent().css("background-color", "#aaaa55");
-		$('input[type="radio"]:not(:checked)').parent().css("background-color", "#ddd");
+		$('input[name="selection"]:checked').parent().css("background-color", "#97c499");
+		$('input[name="selection"]:not(:checked)').parent().css("background-color", "#ddd");
 		ballSpeedX = 6;
 		ballSpeedY = 2;
 		userPaddleHeight = 140;
@@ -46,8 +162,8 @@ $(function(){
 
 	$('.button2').on('click', function(){
 		ballReset();
-		$('input[type="radio"]:checked').parent().css("background-color", "#aaaa55");
-		$('input[type="radio"]:not(:checked)').parent().css("background-color", "#ddd");
+		$('input[name="selection"]:checked').parent().css("background-color", "#97c499");
+		$('input[name="selection"]:not(:checked)').parent().css("background-color", "#ddd");
 		ballSpeedX = 10;
 		ballSpeedY = 5;
 		userPaddleHeight = 100;
@@ -61,8 +177,8 @@ $(function(){
 
 	$('.button3').on('click', function(){
 		ballReset();
-		$('input[type="radio"]:checked').parent().css("background-color", "#aaaa55");
-		$('input[type="radio"]:not(:checked)').parent().css("background-color", "#ddd");
+		$('input[name="selection"]:checked').parent().css("background-color", "#97c499");
+		$('input[name="selection"]:not(:checked)').parent().css("background-color", "#ddd");
 		ballSpeedX = 15;
 		ballSpeedY = 6;
 		userPaddleHeight = 80;
@@ -82,10 +198,13 @@ $(function(){
 		});
 });
 
+// only game functions
+
 function callBoth() {
 	moveEverything();
 	drawEverything();
 }
+
 
 function moveEverything() {
 	if(showingWinScreen == true) {
@@ -118,10 +237,10 @@ function moveEverything() {
 		}
 	}
 
-	if(ballY <0) {
+	if(ballY < (ballWidth / 3)) {
 		ballSpeedY = -ballSpeedY;
 	}
-	if(ballY > canvas.height) {
+	if(ballY > (canvas.height - (ballWidth / 3))) {
 		ballSpeedY = -ballSpeedY;
 	}
 }
@@ -133,18 +252,23 @@ function drawNet() {
 }
 
 function drawEverything() {
-	colorRect(0,0,canvas.width,canvas.height, 'black'); // blanks out the screen
+	colorRect(0,0,canvas.width,canvas.height, 'black'); 	// blanks out the screen
 
-
+	var background = new Image();
+	background.src = backgroundPattern;
+	canvasContext.drawImage(background,0,0,canvas.width,canvas.height);   
 
 	if(showingWinScreen == true) {
 		canvasContext.fillStyle = 'white';
 		if(player1Score >= WINNING_SCORE){
-			canvasContext.fillText("Player 1 wins!!!", 300, 300);
+			canvasContext.font = "30px Orbitron";
+			canvasContext.fillText("You win!!!", 300, 300);
 		} else if(player2Score >= WINNING_SCORE){
-			canvasContext.fillText("Player 2 wins!!!", 300, 300);
+			canvasContext.font = "30px Orbitron";
+			canvasContext.fillText("You lose", 300, 300);
 		}
 
+		canvasContext.font = "25px Orbitron";
 		canvasContext.fillText("click to continue", 300, 400);
 		return; // fuction drawEverything is not going to work further
 	}
@@ -154,8 +278,9 @@ function drawEverything() {
 	colorRect(canvas.width-PADDLE_WIDTH, paddle2Y, PADDLE_WIDTH, computerPaddleHeight, 'white'); // draw right paddle
 	drawNet();
 	// draw ball
-	colorCircle(ballX, ballY, 10, 'white');
+	colorCircle(ballX, ballY, 10, ballColor);
 
+	canvasContext.font = "30px Arial";
 	canvasContext.fillText(player1Score, 100, 100);
 	canvasContext.fillText(player2Score, canvas.width-100, 100);
 }
